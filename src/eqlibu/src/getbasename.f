59cde3096f38d1487679ac7baa1602db74d3a4b9
Douglas G. Moore <doug@39alpharesearch.org>
2025-05-15 15:58:22 -0700

    Identify file extensions using the final fullstop

--- src/eqlibu/src/getbasename.f
+++ src/eqlibu/src/getbasename.f
@@ -2,24 +2,18 @@
             implicit none
             character(*), intent(in) :: filename
             integer, intent(out) :: indices(2)
-            integer :: n, temp, slashtemp, extindex, slashindex
+            integer :: n, i, extindex, slashindex
 
             n = len(filename)
-            temp = index(filename, '/')
-            slashtemp = 0
             slashindex = 0
-            do while (temp.ne.0 .and. slashtemp.ne.n)
-                slashtemp = slashtemp + temp
-                slashindex = slashtemp
-                temp = index(filename(slashtemp+1:), '/')
+            extindex = n + 1
+            do i = 1, n
+                if (filename(i:i).eq.'/') then
+                    slashindex = i
+                else if (filename(i:i).eq.'.') then
+                    extindex = i
+                end if
             end do
-
-            extindex = index(filename(slashindex+1:), '.')
-            if (extindex.eq.0) then
-                extindex = n + 1
-            else
-                extindex = slashindex + extindex
-            end if
 
             indices(1) = slashindex + 1
             indices(2) = extindex - 1
